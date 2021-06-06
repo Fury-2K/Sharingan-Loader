473f449fbbd09439bca56e63dca06503fa0d479d
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-06-06 16:05:41 +0530

    Add sharingan loader

---
+++ Sources/SharinganLoader/Extension+Color.swift
@@ -0,0 +1,12 @@
+//
+//  Extension+Color.swift
+//  ShariganLoader
+//
+//  Created by Manas Aggarwal on 28/05/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let shariRed = Color("shariRed")
+}
